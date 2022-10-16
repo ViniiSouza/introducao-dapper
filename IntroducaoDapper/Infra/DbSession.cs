65fc5e4520cb3c1014b6f82de6ad01822abdafdc
Vinícius <vinigsouza.bnu@gmail.com>
2022-10-15 21:16:26 -0300

    Implementação da DbSession e appsettings

---
+++ IntroducaoDapper/Infra/DbSession.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace IntroducaoDapper.Infra
+{
+    public class DbSession : IDisposable
+    {
+        public IDbConnection Connection { get; set; }
+
+        public DbSession(IConfiguration configuration)
+        {
+            Connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+
+            Connection.Open();
+        }
+
+        public void Dispose() => Connection?.Dispose();
+    }
+}
